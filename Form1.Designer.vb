e7a54d8a4d1b2b9f55d8a042fb3840d7057ab908
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2024-02-12 11:43:18 +0100

    Mise en place du navigateur

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class Navigateur
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,16 +22,90 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        ToolStripMenuItem2 = New ToolStripMenuItem()
+        ToolStripMenuItem3 = New ToolStripMenuItem()
+        ToolStripMenuItem1 = New ToolStripMenuItem()
+        ToolStripMenuItem4 = New ToolStripMenuItem()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        MenuStrip1.SuspendLayout()
         SuspendLayout()
         ' 
-        ' Form1
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {ToolStripMenuItem2, ToolStripMenuItem3, ToolStripMenuItem1, ToolStripMenuItem4})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(1017, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' ToolStripMenuItem2
+        ' 
+        ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        ToolStripMenuItem2.Size = New Size(72, 20)
+        ToolStripMenuItem2.Text = "Précédent"
+        ' 
+        ' ToolStripMenuItem3
+        ' 
+        ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        ToolStripMenuItem3.Size = New Size(58, 20)
+        ToolStripMenuItem3.Text = "Suivant"
+        ' 
+        ' ToolStripMenuItem1
+        ' 
+        ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        ToolStripMenuItem1.Size = New Size(56, 20)
+        ToolStripMenuItem1.Text = "Quitter"
+        ' 
+        ' ToolStripMenuItem4
+        ' 
+        ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        ToolStripMenuItem4.Size = New Size(43, 20)
+        ToolStripMenuItem4.Text = "Aide"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Dock = DockStyle.Top
+        TextBox1.Location = New Point(0, 24)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(1017, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Dock = DockStyle.Top
+        Button1.Location = New Point(0, 47)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(1017, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Rechercher"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Navigateur
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1017, 663)
-        Name = "Form1"
-        Text = "Form1"
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Navigateur"
+        Text = "Navigateur Web VB"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem4 As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,19 @@
-﻿Public Class Form1
+﻿Public Class Navigateur
+    Private Sub MenuStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStrip1.ItemClicked
+        If e.ClickedItem.Text = "Quitter" Then
+            Application.Exit()
+        ElseIf e.ClickedItem.Text = "Précédent" Then
 
+        ElseIf e.ClickedItem.Text = "Suivant" Then
+
+        ElseIf e.ClickedItem.Text = "Aide" Then
+            MessageBox.Show("Logiciel développé par Matthieu Bonjour - Version 1.0", "Aide")
+        End If
+    End Sub
+
+    Private Sub Navigateur_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim web As New WebBrowser
+        web.Dock = DockStyle.Fill
+        Me.Controls.Add(web)
+    End Sub
 End Class
